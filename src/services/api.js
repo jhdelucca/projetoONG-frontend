f8e788fc78bda422b66d653175ed04ae0f16983d
João Henrique Delucca Barros <jhdbarros@hotmail.com>
2020-04-08 12:14:09 -0300

    link da api backend heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
 const api = Axios.create({
-    baseURL: 'http://localhost:3333/',
+    //baseURL: 'http://localhost:3333/',
+      baseURL:'https://ong-backend.herokuapp.com/'
 })
 
 export default api;
